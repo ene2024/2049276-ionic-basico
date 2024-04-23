e68734afd46261f9eee44ced33bde9e5fa7d917a
Karla <karla.cantub@uanl.edu.mx>
2024-04-23 09:28:48 -0600

    Práctica 9: Servicios en Angular

--- miApp-2049276/src/app/app.module.ts
+++ miApp-2049276/src/app/app.module.ts
@@ -6,12 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
 import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';
+import { ProductosComponent } from './productos/productos.component';
 
 @NgModule({
-  declarations: [AppComponent, ListaAlumnosComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent, ListaAlumnosComponent, ProductosComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
